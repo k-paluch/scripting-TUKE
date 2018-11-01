6c97a91efc4e4d3f738f37db424d569f4833477d
Kristián Paľuch <kristian.paluch@student.tuke.sk>
2018-11-01 12:29:15 +0100

    final

--- regexp.sh
+++ regexp.sh
@@ -40,7 +40,7 @@ egrep -o "\(pocitac\)" $DATA5 # RE5
 echo "--- RE6 ---"
 egrep -o "Poci\*tac" $DATA6 # RE6
 echo "--- RE7 ---"
-egrep -o "Poci(\\)(\\)tac" $DATA7 # RE7
+egrep -o "Poci[\][\]tac" $DATA7 # RE7
 echo "--- RE8 ---"
 egrep -o "\"pocitac\"" $DATA8 # RE8
 echo "--- RE9 ---"
@@ -50,36 +50,36 @@ egrep -o "Poc\+itac\+\+" $DATA10 # RE10
 echo "--- RE11 ---"
 egrep -o "p\?ocitac\?" $DATA11 # RE11
 echo "--- RE12 ---"
-egrep -o "''výpočtová technika'|'počítač'" $DATA12 # RE12
+egrep -o "(výpočtová technika)|(počítač)" $DATA12 # RE12
 echo "--- RE13 ---"
-egrep -o "'POČÍTAČ'|'počítač'" $DATA13 # RE13
+egrep -o "(POČÍTAČ)|(počítač)"$DATA13 # RE13
 echo "--- RE14 ---"
-egrep -o "'počítač'|'Počítač'" $DATA14 # RE14
+egrep -o "(Počítač)|(počítač)" $DATA14 # RE14
 echo "--- RE15 ---"
-egrep -o "'počítač'/i" $DATA15 # RE15
+egrep -o -i "'počítač'" $DATA15 # RE15
 echo "--- RE16 ---"
-egrep -o "^'počítač'.*" $DATA16 # RE16
+egrep -o "^počítač.{0,}" $DATA16 # RE16
 echo "--- RE17 ---"
-egrep -o "^.*'počítač'+$" $DATA17 # RE17
+egrep -o ".{0,}počítač$" $DATA17 # RE17
 echo "--- RE18 ---"
-egrep "\b[0-9][a-zA-Z0-9]" $DATA18 # RE18
+egrep '(\b[0-9])\w+\b' $DATA18 # RE18
 echo "--- RE19 ---"
-egrep "\b[a-zA-Z0-9]*(?=de|te|ne|le|di|ti|ni|li)[a-zA-Z0-9]\w*" $DATA19 # RE19
+egrep "\b\w*(de|te|ne|le|di|ti|ni|li)\w*\b" $DATA19 # RE19
 echo "--- RE20 ---"
-egrep "" $DATA20 # RE20
+egrep "(0|[1-9][0-9]*)(L|l)?" $DATA20 # RE20
 echo "--- RE21 ---"
-egrep "" $DATA21 # RE21
+egrep " 0[0-7]*(L|l)?" $DATA21 # RE21
 echo "--- RE22 ---"
-egrep "" $DATA22 # RE22
+egrep "[1-9][0-9]{2}(l|L)?" $DATA22 # RE22
 echo "--- RE23 ---"
-egrep "\w*(\w)(\1+)\w*" $DATA23 # RE23
+egrep "(.)\1{1,}" $DATA23 # RE23
 echo "--- RE24 ---"
-egrep "\b(\w+)\s+\1\b" $DATA24 # RE24
+egrep  "\b(\w+)(\W+\1\b)+" $DATA24 # RE24
 echo "--- RE25 ---"
-egrep "\w[a-zA-Z]+\.[a-zA-Z]+(\.[1-9])?@(student\.)?(tuke\.sk)" $DATA25 # RE25
+egrep "[a-zA-Z]+\.[a-zA-Z]+(\.[1-9][0-9]*)?@(student\.)?tuke\.sk" $DATA25 # RE25
 echo "--- RE26 ---"
-egrep "\w([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]\.[0-9][0-9][0-9]" $DATA26 # RE26
+egrep "([[:blank:]][0-1][0-9]\:[0-5][0-9]\:[0-5][0-9]\.[0-9]{3}[[:blank:]])|([[:blank:]]2[0-3]\:[0-5][0-9]\:[0-5][0-9]\.[0-9]{3}[[:blank:]])" $DATA26 # RE26
 echo "--- RE27 ---"
 egrep "^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(:0[48]|[2468][048]|[13579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(" $DATA27 # RE27
 echo "--- RE28 ---"
-egrep "" $DATA28 # RE28
+egrep "\b\w*(\w{2})\w*\1" $DATA28 # RE28
