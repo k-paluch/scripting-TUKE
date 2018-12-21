8442984e1d27fe5661ad89ab38970409a5fd0075
Kristián Paľuch <kristian.paluch@student.tuke.sk>
2018-12-21 12:17:53 +0100

    Replace skript05.sh

--- skript05.sh
+++ skript05.sh
@@ -1,38 +1,31 @@
-
 #!/bin/bash
-if [ -z "$1"]
+if [ -z "$1" ]||[ "$2" ]
 	then
 		echo "ZLY POCET PARAMETROV. KONCIM." >&2
 		exit 20
-fi
-if ["$2"]
-	then 
-		echo "ZLY POCET PARAMETROV. KONCIM." >&2
-		exit 20
-fi
-DIRECTORY=$1
-if [ ! -d "$DIRECTORY"]
+
+elif [ ! -d "$1" ]
 	then
 		echo "NEPLATNY ADRESAR. KONCIM." >&2
 		exit 30;
 fi 
-cd $DIRECTORY;
-for filename in $(ls -Sr); do
+cd $1;
+for file in $(ls -Sr); do
 if [-f $filename]; 
 	then linescount=0
 		while read line; do
-			if [["$line" =~ ^([a-z]|[ ])+$ ]]
-			then linescount=$((linescount + 1))
+			if [[ "$line" =~ ^([a-z]|[ ])+$ ]]
+			then 
+				linescount=$((linescount+1))
 			fi
 
-		done <$filename
-		if [$linescount -gt 0]; 
+		done <$file
+		if [$linescount -eq 0]; 
 			then
-				echo $filename$'\t'ano >&2
+				echo $file$'\t'nie >&2
 			else
-				echo $filename$'\t'nie >&2
+				echo $file$'\t'ano >&2
 		fi
-			echo $filename$'\t'$linescount
+			echo $file$'\t'$linescount >$DIRECTORY
 fi
-
 done
