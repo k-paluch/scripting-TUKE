e74d21e725ebfd2af959de74815216ad4e2448e3
Kristián Paľuch <kristian.paluch@student.tuke.sk>
2019-01-08 09:01:01 +0100

    Update skript05.sh

--- skript05.sh
+++ skript05.sh
@@ -9,23 +9,25 @@ elif [ ! -d "$1" ]
 		echo "NEPLATNY ADRESAR. KONCIM." >&2
 		exit 30;
 fi 
-cd $1;
-for file in $(ls -Sr); do
-if [ -f $filename ]; 
-	then linescount=0
-		while read line; do
-			if [[ "$line" =~ ^([a-z]|[ ])+$ ]]
-			then 
-				linescount=$((linescount+1))
-			fi
 
-		done <$file
-		if [ $linescount -eq 0 ]; 
-			then
-				echo -e "$file"$'\t'nie >&2
-			else
-				echo -e "$file"$'\t'ano >&2
-		fi
-			echo -e "$file"$'\t'$linescount >$1
-fi
+for i in "$1"/*; do
+    if test -f "$i"; then
+	file=`echo $i | rev | cut -d "/" -f 1 | rev`
+	echo  $file'\t'`cat "$i" | grep "^[[:space:]]*[a-z]*[[:space:]]*[a-z]*$" | wc -l`
+    fi
 done
+# vypis ci sa v danom riadku nachadzaju riadky odpovedajuce danemu regularnemu vyrazu
+for i in "$1"/*; do
+    if test -f "$i"; then
+	file=`echo $i | rev | cut -d "/" -f 1 | rev`
+	if [ `cat "$i" | grep "^[[:space:]]*[a-z]*[[:space:]]*[a-z]*$" | wc -l` -eq 0 ]; then
+		>&2 printf "$file\tnie\n"
+	else
+		>&2 printf "$file\tano\n"
+	fi
+    fi
+done
+
+exit 0;
+
+
