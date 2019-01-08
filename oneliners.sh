efa2fa5d803e298940b7384e7a83a328c87f7f1a
Kristián Paľuch <kristian.paluch@student.tuke.sk>
2019-01-08 08:59:58 +0100

    Update oneliners.sh

--- oneliners.sh
+++ oneliners.sh
@@ -6,19 +6,19 @@ DATA4="/usr/sbin:/usr/bin:/sbin:/bin:/usr/games::/usr/local/games"
 DATA5=OLDATA
 DATA6=OLDATA/access.log
 echo "--- OL1 ---"
-cat $DATA1 | fold -w 1 | sort | uniq | tr -d '\n' | awk '{ print; }' #OL1
+cat $DATA1 |sed 's/\(.\)/\1\n/g' | sort | uniq | tr -d '\n'|sed -e '$a\' # OL1
 
 echo "--- OL2 ---"
 cat $DATA2 | tr -d '\r' | awk '{ print; }' #OL2
 
 echo "--- OL3 ---"
-cat $DATA3 | awk -F $'\t' '{ t = $2; $2 = $3; $3 = t; print; }' OFS=$'\t' #OL3
+awk 'BEGIN{FS="\t"}{OFS="\t"}$3!=NULL{tmp=$2;$2=$3;$3=tmp;print}$3==NULL{print}' $DATA3 # OL3
 
 echo "--- OL4 ---"
 grep -Eo . <<< $DATA4 | tr -d "\n" | tr -s ":" | tr ":" '\n' && echo "" #OL4
 
 echo "--- OL5 ---"
-cd $DATA5; ls -l -f *.txt *.html *.md | awk '{ total += $5 }; END{ print total }'; cd ..; #OL5
+ls -lp $DATA5 | egrep -v /    |  egrep ".*(\.txt|\.md|\.html)$"    |  awk '{print $5}'       |  awk '{ sum += $1} END {print sum}' #OL5
 
 echo "--- OL6 ---"
 cat $DATA6 | sort | awk '{ print $1; }' | tr " " "\n" | sort | uniq -c | awk '{ printf("%s\t%s\n",$2,$1)}' | sort -k2 -n #OL6
